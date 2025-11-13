00e49bdf9c5164889bf57596e7baa8ffde0b8a39
Herman Tsago <htsago@studenten.hs-bremerhaven.de>
2025-11-13 02:45:32 +0100

    change llm model

--- porto-backend/app/core/constants.py
+++ porto-backend/app/core/constants.py
@@ -2,7 +2,7 @@ INDEX_PATH = "portfolio-db"
 DATABASE_PATH = "data/portfolio-knowledge.json"
 
 DEFAULT_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct"
-DEFAULT_TEMPERATURE = 0.7
+DEFAULT_TEMPERATURE = 0.5
 RETRIEVAL_K = 2
 STREAM_DELAY = 0.01
 
